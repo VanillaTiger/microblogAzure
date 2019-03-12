eb2ff8467a39895ac2f2c6c77cd317c307ec8ae5
VanilaTiger <a.szummer@gmail.com>
2019-03-12 16:24:04 +0100

    mysql DB connection

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('MYSQLCONNSTR_localdb')
+                              #\or \
+        #'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://Adam:adam@127.0.0.1:3306/mydb"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
